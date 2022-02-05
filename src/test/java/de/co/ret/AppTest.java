937629268c99732c9ed42529282d2f466cf73f3a
Jan Wedel <jan.wede@gmail.com>
2022-02-05 14:06:42 +0100

    Adds display name generator

--- src/test/java/de/co/ret/AppTest.java
+++ src/test/java/de/co/ret/AppTest.java
@@ -1,12 +1,15 @@
 package de.co.ret;
 
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class AppTest
 {
     @Test
-    public void should()
+    public void should_greet_on_week_day()
     {
     }
 }
